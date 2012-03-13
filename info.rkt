5ef8b6e141c44d3fdfb3835d94ad5d8cbed0f15c
John Clements <clements@racket-lang.org>
2012-03-13 09:48:13 -0700

    oops info.rkt change

--- info.rkt
+++ info.rkt
@@ -9,7 +9,7 @@
 (define categories '(xml))
 (define compile-omit-paths '("tests/" "ssax/tests/"))
 (define repositories '("4.x"))
-(define release-notes (list "releasing changes from Ryan Culpepper"))
-(define version "2012-01-03 20:25")
+(define release-notes (list "bug fixes, serializers can now accept paths"))
+(define version "2012-02-16 10:11")
 
 (define scribblings '(("scribblings/sxml.scrbl" (multi-page))))
